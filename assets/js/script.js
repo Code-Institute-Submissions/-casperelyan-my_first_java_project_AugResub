8daaa08cb2cf69ed2c80eff6aa149815d7cde486
Lucas <4h4photography@gmail.com>
2022-07-03 22:45:59 +0000

    Innitial Java add, more styles add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
 console.log("Welcome to flag chalange");
+
+const startButton =document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
+const questionContainerElement = document.getElementById('question-container')
+
+
+startButton.addEventListener("click", startQuiz)
+
+
+function startQuiz() {
+    startButton.classList.add('hide')
+   
+}
+
+function setNextQuestion(){
+}
+
+function selectAnswer(){
+
+}
