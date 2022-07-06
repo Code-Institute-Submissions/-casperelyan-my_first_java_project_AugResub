5d88ac1ba1400c5db991a4858aee847ce5830e43
Lucas <4h4photography@gmail.com>
2022-07-06 21:08:15 +0000

    Start function edit, script src moved, css modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,25 @@
-console.log("Welcome to flag chalange");
 
 
-const nextButton = document.getElementById("next-btn")
-const questionContainerElement = document.getElementById('question-container')
-const startButton =document.getElementById("start-btn")
 
-startButton.addEventListener("click", startQuiz)
+const nextButton = document.getElementById('next')
+const questionContainerElement = document.getElementById('questions-div')
+const startButton =document.getElementById('start')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+startButton.addEventListener('click', startPlay)
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++
   setNextQuestion()
 })
 
-
-
-function startQuiz() {
-    startButton.classList.add('hide')
-        shuffledQuestions = questions.sort(() => Math.random() - .5)
-        currentQuestionIndex = 0
-        questionContainerElement.classList.remove('hide')
-        setNextQuestion()
-      }
-   
-      
+function startPlay() {
+  startButton.classList.add('hiden')
+  shuffledQuestions = questions.sort(() => Math.random() - .2)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hiden')
+  setNextQuestion()
+}
 
 function setNextQuestion(){
     resetState()
@@ -30,129 +28,74 @@ function setNextQuestion(){
 
 
 
-function selectAnswer(){
+  function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+  }
+  
+  function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
+  }
+  
+  function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+  }
+  
+  function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide')
+    } else {
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+    }
+  }
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
 
-}
 
-
-
-const questions = [
+  const questions = [
     {
-      question: '',
-      answers: [
-        { text: '', correct:  },
-        { text: '', correct:  }
-        { text: '', correct:  },
-        { text: '', correct:  }
-      ]
-    },
+      question: 'A',
+      answers: 
+        { text: 'B', correct: true },
+        { text: 'C', correct: false },
+        { text: 'D', correct: true },
+        { text: 'F', correct: true },
+      
+    }
+  ]
+      
 
-    {
-        question: '',
-        answers: [
-          { text: '', correct:  },
-          { text: '', correct:  }
-          { text: '', correct:  },
-          { text: '', correct:  }
-        ]
-      },
-
-      {
-        question: '',
-        answers: [
-          { text: '', correct:  },
-          { text: '', correct:  }
-          { text: '', correct:  },
-          { text: '', correct:  }
-        ]
-      },
-
-      {
-        question: '',
-        answers: [
-          { text: '', correct:  },
-          { text: '', correct:  }
-          { text: '', correct:  },
-          { text: '', correct:  }
-        ]
-      },
-
-      {
-        question: '',
-        answers: [
-          { text: '', correct:  },
-          { text: '', correct:  }
-          { text: '', correct:  },
-          { text: '', correct:  }
-        ]
-      },
-
-      {
-        question: '',
-        answers: [
-          { text: '', correct:  },
-          { text: '', correct:  }
-          { text: '', correct:  },
-          { text: '', correct:  }
-        ]
-      },
-
-      {
-        question: '',
-        answers: [
-          { text: '', correct:  },
-          { text: '', correct:  }
-          { text: '', correct:  },
-          { text: '', correct:  }
-        ]
-      },
-
-      {
-        question: '',
-        answers: [
-          { text: '', correct:  },
-          { text: '', correct:  }
-          { text: '', correct:  },
-          { text: '', correct:  }
-        ]
-      },
-
-      {
-        question: '',
-        answers: [
-          { text: '', correct:  },
-          { text: '', correct:  }
-          { text: '', correct:  },
-          { text: '', correct:  }
-        ]
-      },
-
-      {
-        question: '',
-        answers: [
-          { text: '', correct:  },
-          { text: '', correct:  }
-          { text: '', correct:  },
-          { text: '', correct:  }
-        ]
-      },
-
-      {
-        question: '',
-        answers: [
-          { text: '', correct:  },
-          { text: '', correct:  }
-          { text: '', correct:  },
-          { text: '', correct:  }
-        ]
-      },
-
-      {
-        question: '',
-        answers: [
-          { text: '', correct:  },
-          { text: '', correct:  }
-          { text: '', correct:  },
-          { text: '', correct:  }
-        ]
-      },]
+    
