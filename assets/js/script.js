cc55a4e51cc4d31ab3e11de5f624fcefc48f0406
Lucas <4h4photography@gmail.com>
2022-07-13 21:40:25 +0000

    variables css add, new questions add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,103 @@
-
-
-
-const nextButton = document.getElementById('next')
-const questionContainerElement = document.getElementById('questions-div')
-const startButton =document.getElementById('start')
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
-startButton.addEventListener('click', startPlay)
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++
   setNextQuestion()
 })
 
-function startPlay() {
-  startButton.classList.add('hiden')
-  shuffledQuestions = questions.sort(() => Math.random() - .2)
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
   currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hiden')
+  questionContainerElement.classList.remove('hide')
   setNextQuestion()
 }
 
-function setNextQuestion(){
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-  }
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
 
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
 
-  function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
   }
-  
-  function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-      const button = document.createElement('button')
-      button.innerText = answer.text
-      button.classList.add('btn')
-      if (answer.correct) {
-        button.dataset.correct = answer.correct
-      }
-      button.addEventListener('click', selectAnswer)
-      answerButtonsElement.appendChild(button)
-    })
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
   }
-  
-  function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
-  }
-  
-  function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-      setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-      nextButton.classList.remove('hide')
-    } else {
-      startButton.innerText = 'Restart'
-      startButton.classList.remove('hide')
-    }
-  }
-  
-  function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-      element.classList.add('correct')
-    } else {
-      element.classList.add('wrong')
-    }
-  }
-  
-  function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
 
 
   const questions = [
     {
-      question: 'A',
-      answers: 
-        { text: 'B', correct: true },
-        { text: 'C', correct: false },
-        { text: 'D', correct: true },
-        { text: 'F', correct: true },
-      
-    }
+      imageURL:"https://www.countryflags.com/wp-content/uploads/cameroon-flag-png-large.png",
+      answers: [
+        { text: 'Mongolia', correct: true },
+        { text: 'Chad', correct: false },
+        { text: 'Maroko', correct: true },
+        { text: 'Finland', correct: true },
+      ]
+    },
+
+    {
+      imageURL:"https://www.countryflags.com/wp-content/uploads/cameroon-flag-png-large.png",
+      answers: [
+        { text: 'Mongolia', correct: true },
+        { text: 'Chad', correct: false },
+        { text: 'Maroko', correct: true },
+        { text: 'Finland', correct: true },
+      ]
+    },
+
   ]
       
 
-    
